a6f1a487f94e6d0b445f208d9e915172f364c30d
Thegitfiddler <126429041+Thegitfiddler@users.noreply.github.com>
2026-04-22 12:09:39 -0500

    Configure S3 backend for Terraform state management

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,1 +1,8 @@
-test
+terraform {
+  backend "s3" {
+    bucket         = "your-tf-state-bucket"
+    key            = "guardduty/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+  }
+}
