f7bb190d113b142f0c49f67105cf9b0b9434ade2
Thegitfiddler <126429041+Thegitfiddler@users.noreply.github.com>
2026-04-22 12:12:39 -0500

    Define AWS region, project name, and alert email variables
    Added variables for AWS region, project name, and alert email.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,12 @@
-test
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "project_name" {
+  default = "guardduty-auto-response"
+}
+
+variable "alert_email" {
+  description = "Email for SNS notifications"
+  type        = string
+}
